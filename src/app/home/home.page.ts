0a8ce69af61b3af0b140c57c7afb0b231fe217f5
Alessandra Donnini <a.donnini@etcware.it>
2019-11-27 17:07:41 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  map: Map;
 
   constructor() {}
+
+  private loadmap() {
+    this.map = new Map('map').setView([41.84959,12.57446], 8);
+
+    tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWxlbmRvIiwiYSI6ImNrM2ViaXVyZjFlNmszYnFqMHB0bmFmbGcifQ.JHTD6PbUodS4XngjM53FUQ', {
+       // tslint:disable-next-line
+      attribution: "© Mapbox, © OpenStreetMap",
+      maxZoom: 18
+    }).addTo(this.map);
+  }
+
+  private ionViewDidEnter() {
+        this.loadmap();
+  }
 
 }
